aca4bb7d82f53fe2fb53e97ea1d81971219e4659
Abidoyehs <sofiyyah.abidoye@sterling.ng>
2025-01-22 11:06:05 +0100

    first commit

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,9 @@
+// import { Router, Request, Response } from 'express';
+
+// const usersRouter = Router();
+
+// usersRouter.get('/', (request:Request, response: Response) => {
+//     return response.send("ok");
+// });
+
+// export default usersRouter;
